7ea77acb1c65c6ea72113ff4fb18fed0aa76d187
AleKoure <akourets@gmail.com>
2022-11-01 13:31:34 +0000

    GET file component sets endpoint

--- R/get_file_component_sets.R
+++ R/get_file_component_sets.R
@@ -1,12 +1,12 @@
-#' GET file components
+#' GET file component sets
 #'
-#' @description Get a list of published components within a file library.
+#' @description Get a list of published component_sets within a file library.
 #' Note that published components are only available via the professional
 #' plan.
 #'
 #' @param file_key string. Id of the team to list components from.
 #'
-#' @returns S3 object of class `rigma_get_file_components`. Components are
+#' @returns S3 object of class `rigma_get_file_component_sets`. Components are
 #' stored in the `meta` field.
 #'
 #' @importFrom httr2 request req_url_path_append req_headers req_user_agent
@@ -18,16 +18,16 @@
 #' \donttest{
 #' #navigate to team page and get id from url
 #' file_key <- "gYRjH0y8ZM0VtEfO8kf6ch"
-#' get_file_components(file_key)
+#' get_file_component_sets(file_key)
 #' }
 #'
 #' @export
-get_file_components <- function(file_key) {
+get_file_component_sets <- function(file_key) {
   assert_string(file_key)
 
   resp <- request("https://api.figma.com/v1/files/") %>%
     req_url_path_append(file_key) %>%
-    req_url_path_append("components") %>%
+    req_url_path_append("component_sets") %>%
     req_error(body = function(resp) {
       resp %>%
         resp_body_json() %>%
@@ -44,6 +44,6 @@ get_file_components <- function(file_key) {
       meta = resp %>% chuck("meta"),
       i18n = resp %>% chuck("i18n")
     ),
-    class = "rigma_get_file_components"
+    class = "rigma_get_file_component_sets"
   )
 }
