360babe9adff1402f31dfeb817b9032a36d1637d
AleKoure <akourets@gmail.com>
2022-11-23 23:17:24 +0000

    Test updates

---
+++ tests/testthat/test-extract_bslib_palette.R
@@ -0,0 +1,13 @@
+test_that("Extract bslib palette is OK!", {
+  file_key <- "sFHgQh9dL6369o5wrZHmdR"
+  bslib_vars <- file_key %>%
+    get_file_styles() %>%
+    as_design_tibble() %>%
+    add_color() %>%
+    extract_bslib_palette()
+  expect_true(
+    all(
+      names(bslib_vars) %in% names(rlang::fn_fmls(bslib::bs_theme))
+    )
+  )
+})
