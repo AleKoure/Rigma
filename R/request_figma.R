d220ecc019ec12f055ddb3f59b85e5f2fad295d8
Eli Pousson <eli.pousson@gmail.com>
2024-11-20 23:26:44 -0500

    Fix documentation for `request_figma()`

--- R/request_figma.R
+++ R/request_figma.R
@@ -1,13 +1,12 @@
 #' Create a request for the Figma API with an optional method
 #'
-#' Use httr2::request() to create a request with error handling an optionally
+#' Use [httr2::request()] to create a request with error handling an optionally
 #' specified method.
 #'
 #' @rdname request_figma
-#' @param name Base URL for request. Defaults to `"https://api.figma.com"`
+#' @param base_url Base URL for request. Defaults to `"https://api.figma.com"`
 #' @inheritParams httr2::req_method
 #' @inheritParams req_rigma_agent
-#' @inheritDotParams httr2::req_template
 #' @keywords internal
 #' @importFrom httr2 request req_error req_method
 #' @importFrom rlang is_null
